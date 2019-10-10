1790a291d0aea33cbe134596b61860621926c7f7
Kennedy Martin <kennedyjo21@gmail.com>
2019-10-10 15:23:44 -0700

    add final touches to US shipping options

--- js/scripts.js
+++ js/scripts.js
@@ -12,24 +12,99 @@ $(document).ready(function(){
   var value = parseInt($("#order-value").val());
   var isolates = parseInt($("#isolates").val());
   var carts = parseInt($("#cartridges").val());
+  var bottleSize = parseInt($("#bottles").val());
 
     if (value >= 100 && isolates === 1 && carts === 1) {
-      $('#cartsIsolates').show(); 
-      $('#isolatesUS').hide(); 
+        $('#cartsIsolates').show(); 
+        $('#isolatesUS').hide(); 
+        $('#noSpecials').hide();
+        $('#cartsUs').hide();
+        $('#isolatesLarge').hide();
+        $('#isolatesSmall').hide();
+        $('#under100Large').hide();
+        $('#under100Small').hide();
     } else if (value >= 100 && isolates === 1 && carts === 2) {
-      $('#isolatesUs').show();
-      $('#cartsIsolates').hide(); 
+        $('#isolatesUs').show();
+        $('#cartsIsolates').hide();
+        $('#cartsUs').hide();
+        $('#noSpecials').hide();
+        $('#isolatesLarge').hide();
+        $('#isolatesSmall').hide();
+        $('#under100Large').hide();
+        $('#under100Small').hide(); 
     } else if (value >= 100 && isolates === 2 && carts === 2) {
         $('#noSpecials').show();
         $('#isolatesUs').hide();
         $('#cartsIsolates').hide();
+        $('#cartsUs').hide();
+        $('#isolatesLarge').hide();
+        $('#isolatesSmall').hide();
+        $('#under100Large').hide();
+        $('#under100Small').hide();
     } else if (value >= 100 && isolates === 2 && carts === 1) {
         $('#cartsUs').show();
         $('#noSpecials').hide();
         $('#isolatesUs').hide();
         $('#cartsIsolates').hide();
-    }
-    // } else if (value < 100 && isolates === 1 && carts === 1)
+        $('#isolatesLarge').hide();
+        $('#isolatesSmall').hide();
+        $('#under100Large').hide();
+        $('#under100Small').hide();
+    } else if (value < 100 && isolates === 1 && carts === 1) {
+        $('#cartsIsolates').show();
+        $('#cartsUs').hide();
+        $('#noSpecials').hide();
+        $('#isolatesUs').hide();
+        $('#isolatesLarge').hide();
+        $('#isolatesSmall').hide();
+        $('#under100Large').hide();
+        $('#under100Small').hide();
+    } else if (value < 100 && isolates === 2 && carts === 1) {
+        $('#cartsUs').show();
+        $('#cartsIsolates').hide();
+        $('#noSpecials').hide();
+        $('#isolatesUs').hide();
+        $('#isolatesLarge').hide();
+        $('#isolatesSmall').hide();
+        $('#under100Large').hide();
+        $('#under100Small').hide();
+    } else if (value < 100 && isolates === 1 && carts === 2 && bottleSize === 1) {
+        $('#isolatesSmall').show();
+        $('#cartsUs').hide();
+        $('#cartsIsolates').hide();
+        $('#noSpecials').hide();
+        $('#isolatesUs').hide();
+        $('#isolatesLarge').hide();
+        $('#under100Large').hide();
+        $('#under100Small').hide();
+    } else if (value < 100 && isolates === 1 && carts === 2 && bottleSize === 2) {
+        $('#isolatesLarge').show();
+        $('#isolatesSmall').hide();
+        $('#cartsUs').hide();
+        $('#cartsIsolates').hide();
+        $('#noSpecials').hide();
+        $('#isolatesUs').hide();
+        $('#under100Large').hide();
+        $('#under100Small').hide();
+    } else if (value < 100 && isolates === 2 && carts === 2 && bottleSize === 1) {
+      $('#under100Small').show();
+      $('#under100Large').hide();
+      $('#isolatesLarge').hide();
+      $('#isolatesSmall').hide();
+      $('#cartsUs').hide();
+      $('#cartsIsolates').hide();
+      $('#noSpecials').hide();
+      $('#isolatesUs').hide();
+  } else if (value < 100 && isolates === 2 && carts === 2 && bottleSize === 2) {
+    $('#under100Large').show();
+    $('#under100Small').hide();
+    $('#isolatesLarge').hide();
+    $('#isolatesSmall').hide();
+    $('#cartsUs').hide();
+    $('#cartsIsolates').hide();
+    $('#noSpecials').hide();
+    $('#isolatesUs').hide();
+}
 
   });
 });
